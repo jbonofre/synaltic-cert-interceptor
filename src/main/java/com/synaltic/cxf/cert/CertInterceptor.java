896200fcdef4efc7e24104983f41410d8e27a9cf
Jean-Baptiste Onofré <jbonofre@apache.org>
2017-07-11 17:33:15 +0200

    Add certificate validation

--- src/main/java/com/synaltic/cxf/cert/CertInterceptor.java
+++ src/main/java/com/synaltic/cxf/cert/CertInterceptor.java
@@ -9,10 +9,12 @@ import org.apache.cxf.security.transport.TLSSessionInfo;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.security.InvalidKeyException;
 import java.security.KeyStore;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
-import java.util.List;
+import java.security.PublicKey;
+import java.security.SignatureException;
+import java.security.cert.*;
+import java.util.*;
 
 public class CertInterceptor extends AbstractPhaseInterceptor<Message> {
 
@@ -44,9 +46,68 @@ public class CertInterceptor extends AbstractPhaseInterceptor<Message> {
             KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
             keyStore.load(new FileInputStream(new File(System.getProperty("karaf.etc") + File.pathSeparator + bus.getId())), "password".toCharArray());
 
-            
+            if (!validateKeyChain((X509Certificate) certificate, keyStore)) {
+                throw new SecurityException("Certificate is invalid");
+            }
         } catch (Exception e) {
             throw new SecurityException("Certificate verification failed", e);
         }
     }
+
+    private boolean validateKeyChain(X509Certificate certificate, KeyStore keyStore) throws Exception {
+        X509Certificate[] certificates = new X509Certificate[keyStore.size()];
+        int i = 0;
+        Enumeration<String> alias = keyStore.aliases();
+        while (alias.hasMoreElements()) {
+            certificates[i++] = (X509Certificate) keyStore.getCertificate(alias.nextElement());
+        }
+        return validateKeyChain(certificate, certificates);
+    }
+
+    private boolean validateKeyChain(X509Certificate certificate, X509Certificate... trustedCertificates) throws Exception {
+        boolean found = false;
+        int i = trustedCertificates.length;
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        TrustAnchor anchor;
+        Set anchors;
+        CertPath path;
+        List list;
+        PKIXParameters params;
+        CertPathValidator validator = CertPathValidator.getInstance("PKIX");
+        while (!found && i > 0) {
+            anchor = new TrustAnchor(trustedCertificates[--i], null);
+            anchors = Collections.singleton(anchor);
+            list = Arrays.asList(new Certificate[] { certificate });
+            path = cf.generateCertPath(list);
+            params = new PKIXParameters(anchors);
+            params.setRevocationEnabled(false);
+            if (certificate.getIssuerDN().equals(trustedCertificates[i].getSubjectDN())) {
+                try {
+                    validator.validate(path, params);
+                    if (isSelfSigned(trustedCertificates[i])) {
+                        // found root CA
+                        found = true;
+                    } else if (!certificate.equals(trustedCertificates[i])) {
+                        found = validateKeyChain(trustedCertificates[i], trustedCertificates);
+                    }
+                } catch (Exception e) {
+                    // validate failed, check next cert in the trust store
+                }
+            }
+        }
+        return found;
+    }
+
+    private boolean isSelfSigned(X509Certificate certificate) throws Exception {
+        try {
+            PublicKey key = certificate.getPublicKey();
+            certificate.verify(key);
+            return true;
+        } catch (SignatureException signatureException) {
+            return  false;
+        } catch (InvalidKeyException invalidKeyException) {
+            return false;
+        }
+    }
+
 }
